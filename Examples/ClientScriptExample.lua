1a6fd22e266de6816ef3d006bef0a1644d6b99d6
HiddenKaiser <45469342+HiddenKaiser@users.noreply.github.com>
2023-02-01 18:58:18 -0500

    Update ClientScriptExample.lua

--- Examples/ClientScriptExample.lua
+++ Examples/ClientScriptExample.lua
@@ -2,12 +2,14 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local RemoteParser = require( ReplicatedStorage:WaitForChild("RemoteParser") );
 
-local parser = RemoteParser.new( ReplicatedStorage.Remote );
+local remote = RemoteParser.new( ReplicatedStorage.Remote );
+
+-- method name is passed in the first argument, so that a developer doesn't have to create and save a variable for every single method.
 
 while true do
 	for i = 1,2 do
-		parser:FireServer("Test", "arg1", "arg2", "yes")
+		remote:FireServer("Test", "arg1", "arg2", "yes")
 	end
-	parser:FireServer("Method2", "a", "b")
+	remote:FireServer("Method2", "a", "b")
 	wait(0.5)
 end
