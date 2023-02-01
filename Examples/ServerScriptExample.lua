af6cd466184abd9615d944a7254b5d1a30f67e9c
HiddenKaiser <45469342+HiddenKaiser@users.noreply.github.com>
2023-02-01 18:59:55 -0500

    Update ServerScriptExample.lua

--- Examples/ServerScriptExample.lua
+++ Examples/ServerScriptExample.lua
@@ -2,12 +2,16 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local RemoteParser = require( ReplicatedStorage:WaitForChild("RemoteParser") );
 
-local parser = RemoteParser.new( ReplicatedStorage.Remote );
+local remote = RemoteParser.new( ReplicatedStorage.Remote );
 
-local Connection = parser.On("Test", true):Connect(function(player, arg1, arg2, ...)
+-- remote.On emulates a RbxScriptSignal, so you can use the same methods as you would with a regular event!
+
+-- connection can be saved to a variable
+local Connection = remote.On("Test", true):Connect(function(player, arg1, arg2, ...)
 	warn(player, arg1, arg2, "\nExtra Args: ", ...);
 end)
 
-parser.On("Method2", true):Connect(function(player, ...)
+-- but it doesn't have to be
+remote.On("Method2", true):Connect(function(player, ...)
 	warn(player, "Method 2:", ...);
 end)
