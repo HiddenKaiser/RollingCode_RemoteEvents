8bffea694b0a1a54db18627f3fd57412e5efc15a
HiddenKaiser <45469342+HiddenKaiser@users.noreply.github.com>
2023-02-02 15:26:49 -0500

    Update ClientScriptExample.lua

--- Examples/ClientScriptExample.lua
+++ Examples/ClientScriptExample.lua
@@ -7,9 +7,7 @@ local remote = RemoteParser.new( ReplicatedStorage.Remote );
 -- method name is passed in the first argument, so that a developer doesn't have to create and save a variable for every single method.
 
 while true do
-	for i = 1,2 do
-		remote:FireServer("Test", "arg1", "arg2", "yes")
-	end
+	remote:FireServer("Test", "arg1", "arg2", "yes")
 	remote:FireServer("Method2", "a", "b")
 	wait(0.5)
 end
