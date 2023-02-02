0f24ada9dc3fe7de301edba5915d273370aa08cb
HiddenKaiser <45469342+HiddenKaiser@users.noreply.github.com>
2023-02-02 15:50:55 -0500

    Update ClientScriptExample.lua

--- Examples/ClientScriptExample.lua
+++ Examples/ClientScriptExample.lua
@@ -4,10 +4,16 @@ local RemoteParser = require( ReplicatedStorage:WaitForChild("RemoteParser") );
 
 local remote = RemoteParser.new( ReplicatedStorage.Remote );
 
+remote.On("ClientExample"):Connect(function(arg1, arg2)
+	print("Client says: ", arg1, arg2);
+end)
+
 -- method name is passed in the first argument, so that a developer doesn't have to create and save a variable for every single method.
 
 while true do
-	remote:FireServer("Test", "arg1", "arg2", "yes")
+	for i = 1,1 do
+		remote:FireServer("Test", "arg1", "arg2", "yes")
+	end
 	remote:FireServer("Method2", "a", "b")
 	wait(0.5)
 end
